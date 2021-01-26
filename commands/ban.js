1a90105cf279bffb9236a8169395a1a056c1f503
Zuoers <67604119+Zuoers@users.noreply.github.com>
2021-01-26 21:49:07 +0000

    Update ban.js

--- commands/ban.js
+++ commands/ban.js
@@ -1,13 +1,13 @@
 module.exports.run = async (bot, message, args) => {
     if(!message.member.hasPermission('BAN_MEMBERS')) 
-        message.channel.send("No tienes permiso para ejecutar el comando.");
+        message.channel.send("You do not have access to this command!");
     else {
         let bannedMember = await message.guild.members.ban(args);
         if(bannedMember){
 
         try {
             console.log(bannedMember.tag + " Fue baneado.");
-            message.channel.send (`${bannedMember} Fue baneado del servidor!`)
+            message.channel.send (`${bannedMember} They are now banned!`)
         }
             catch(err) {
             console.log(err);
