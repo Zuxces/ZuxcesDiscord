0a82c0804f1d077eb5d96cd8154d42b04c52467f
Zuoers <67604119+Zuoers@users.noreply.github.com>
2021-05-31 20:23:51 +0100

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -2,5 +2,5 @@ const Discord = require("discord.js")
 
 module.exports = bot => { 
     console.log(`Logged in as ${bot.user.username}`)
-    bot.user.setActivity("zuoers.dev | ?help", {type: "WATCHING"});
+    bot.user.setActivity(";help | v1.4", {type: "WATCHING"});
 }
