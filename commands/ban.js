77f2e999672bd0990f421f07ebce00755d9e8022
Zuoers <67604119+Zuoers@users.noreply.github.com>
2021-06-01 13:13:05 +0100

    Update ban.js

--- commands/ban.js
+++ commands/ban.js
@@ -1,19 +1,23 @@
-module.exports.run = async (bot, message, args) => {
-    if(!message.member.hasPermission('BAN_MEMBERS')) 
-        message.channel.send("You do not have access to this command!");
-    else {
-        let bannedMember = await message.guild.members.ban(args);
-        if(bannedMember){
+exports.run = (client, message, args) => {
+    let member = message.mentions.members.first() || message.guild.members.cache.get(args[0]);
 
-        try {
-            console.log(bannedMember.tag + " Fue baneado.");
-            message.channel.send (`${bannedMember} They are now banned!`)
-        }
-            catch(err) {
-            console.log(err);
-        }
+    if(!message.member.hasPermission("BAN_MEMBERS")){
+        message.channel.send("You don't have the permissions to use this command!");
     }
-}
+
+    else{
+        if(!member)
+            return message.channel.send("Please mention a valid member of this server");
+        if(!member.bannable) 
+            return message.channel.send("I cannot ban this user! Do they have a higher role? Do I have ban permissions?");
+
+        let reason = args.slice(1).join(' ');
+        if(!reason) reason = "No reason provided";
+
+        member.ban({reason: reason})
+            .catch(error => message.channel.send(`Sorry ${message.author} I couldn't ban the user`));
+        message.channel.send(`${member.user.tag} has been banned by ${message.author.tag} because: ${reason}`);
+    }
 }
 
 module.exports.config = {
