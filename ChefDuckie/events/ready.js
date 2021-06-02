d2663cc7d7558e22e14a54b823f78349927cb2a2
Zuoers <67604119+Zuoers@users.noreply.github.com>
2021-06-02 23:45:50 +0100

    Delete ChefDuckie directory

--- ChefDuckie/events/ready.js
+++
@@ -1,6 +0,0 @@
-const Discord = require("discord.js")
-
-module.exports = bot => { 
-    console.log(`Logged in as ${bot.user.username}`)
-    bot.user.setActivity(";help | v3.2", {type: "PLAYING"});
-}
